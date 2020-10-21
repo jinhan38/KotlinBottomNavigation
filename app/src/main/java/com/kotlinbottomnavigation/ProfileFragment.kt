35caebdddeeb57a9cf03f0eb986ae631543a257f
jinhan38 <jinhan38@naver.com>
2020-10-21 19:11:44 +0900

    commit

--- app/src/main/java/com/kotlinbottomnavigation/ProfileFragment.kt
+++ app/src/main/java/com/kotlinbottomnavigation/ProfileFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment() {      
 
     //static
     companion object {
